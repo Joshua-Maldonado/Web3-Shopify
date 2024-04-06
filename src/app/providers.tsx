dbb23d4ddb72c89b65b4c287ff3a2cd1a23428d4
jtaaa <joshuatallum@gmail.com>
2024-04-06 11:52:28 -0400

    chore: Cleanup custom wagmi config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,7 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState, type ReactNode } from 'react'
-import { WagmiProvider } from 'wagmi'
+import { type ReactNode } from 'react'
 import { WalletProvider } from "@opensea/wallet"
-import { createConfig, ConfigParams, Environment } from '../wagmi'
-
-const Env = "production"
-const config = {
-  Environment: Env,
-  supportInjectedWallets: true
-} 
-
-const configWagmi = createConfig(config)
 
 export function Providers(props: { children: ReactNode }) {
 
