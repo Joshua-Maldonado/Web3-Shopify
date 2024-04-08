0176def29b0fef3b04a06546beb4062551f83488
Josh Maldonado <joshm@topdrawermerch.com>
2024-04-08 07:48:55 -0600

    update to env vars ref

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,7 @@ import { WalletProvider } from "@opensea/wallet"
 export function Providers(props: { children: ReactNode }) {
 
   return (
-    <WalletProvider config={{ environment: "production"}}>
+    <WalletProvider config={{ environment: "production "}}>
       {props.children}
     </WalletProvider>
   )
