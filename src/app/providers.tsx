8a4532dca80e64be5bbf179c4e0b914550b5af58
Josh Maldonado <joshm@topdrawermerch.com>
2024-06-25 13:09:43 -0600

    OS Wallet Update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,8 +6,9 @@ import { WalletProvider } from "@opensea/wallet"
 export function Providers(props: { children: ReactNode }) {
 
   return (
-    <WalletProvider environment="production" >
+    <WalletProvider environment="production" clientId="client-WY2cXW6EKKZZQ8CsdbA4WqGwVf2Cjh2ZuGWCQ547iwpQN">
       {props.children}
     </WalletProvider>
   )
 }
+   
